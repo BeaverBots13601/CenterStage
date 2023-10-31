f415eaaec7e85d80253db421b3a16f987384ac27
Alex McKinley <alex.mckinley@intel.com>
2023-10-31 10:11:01 -0700

    Switch from map implementation to array implemenation for drive motors. added enum for drive motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/baseRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/baseRobot.java
@@ -7,18 +7,16 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import java.util.Arrays;
-import java.util.Map;
-import java.util.TreeMap;
 
 @Disabled
 public abstract class baseRobot extends LinearOpMode {
 
-    final TreeMap<String, DcMotorEx> driveMotors;
+    DcMotorEx[] driveMotors;
     private final double wheelDiameter;
     private final double robotDiameter;
 
     public baseRobot(double wheelDiameter, double robotDiameter) {
-        this.driveMotors = new TreeMap<>();
+        this.driveMotors = new DcMotorEx[constants.driveMotorName.values().length];
         this.telemetry.setMsTransmissionInterval(constants.TELEMETRY_MS_TRANSMISSION_INTERVAL);
         createDriveMotors();
 
@@ -41,30 +39,28 @@ public abstract class baseRobot extends LinearOpMode {
     }
 
     private void createDriveMotors() {
-        for (String driveMotorName : constants.DRIVE_MOTOR_NAMES) {
-            DcMotorEx driveMotor = createDefaultMotor(driveMotorName);
-            this.driveMotors.put(driveMotorName, driveMotor);
+        for (constants.driveMotorName driveMotorName : constants.driveMotorName.values()) {
+            DcMotorEx driveMotor = createDefaultMotor(driveMotorName.name());
+            this.driveMotors[driveMotorName.ordinal()] = driveMotor;
         }
     }
 
     public void setDriveMotors(double[] powers, DcMotor.RunMode mode) {
-        int i = 0;
-        for (Map.Entry<String, DcMotorEx> motor : this.driveMotors.entrySet()) {
-            motor.getValue().setMode(mode);
-            motor.getValue().setPower(powers[i]);
-            i++;
+        for (constants.driveMotorName driveMotorName : constants.driveMotorName.values()) {
+            this.driveMotors[driveMotorName.ordinal()].setMode(mode);
+            this.driveMotors[driveMotorName.ordinal()].setPower(powers[driveMotorName.ordinal()]);
         }
     }
 
     public void stopDrive() {
-        double[] powers = new double[this.driveMotors.size()];
+        double[] powers = new double[this.driveMotors.length];
         Arrays.fill(powers, 0.0);
         setDriveMotors(powers, DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     private boolean isDriving() {
-        for (Map.Entry<String, DcMotorEx> motor : this.driveMotors.entrySet()) {
-            if (motor.getValue().isBusy()) {
+        for (DcMotorEx motor : this.driveMotors) {
+            if (motor.isBusy()) {
                 return true;
             }
         }
@@ -76,22 +72,18 @@ public abstract class baseRobot extends LinearOpMode {
     }
 
     public void driveEncoded(int[] ticks, double[] powers) {
-        int i = 0;
-        for (Map.Entry<String, DcMotorEx> motor : this.driveMotors.entrySet()) {
-            motor.getValue().setTargetPosition(ticks[i]);
-            i++;
+        for (constants.driveMotorName driveMotorName : constants.driveMotorName.values()) {
+            this.driveMotors[driveMotorName.ordinal()].setTargetPosition(ticks[driveMotorName.ordinal()]);
         }
 
         this.setDriveMotors(powers, DcMotor.RunMode.RUN_TO_POSITION);
 
         while (opModeIsActive() && this.isDriving()) {
-            i = 0;
-            for (Map.Entry<String, DcMotorEx> motor : this.driveMotors.entrySet()) {
-                telemetry.addData("Running to", " %7d", ticks[i]);
+            for (constants.driveMotorName driveMotorName : constants.driveMotorName.values()) {
+                telemetry.addData("Running to", " %7d", ticks[driveMotorName.ordinal()]);
                 telemetry.addData("Currently at", "%s at %7d",
-                        motor.getKey(),
-                        motor.getValue().getCurrentPosition());
-                i++;
+                        driveMotorName.name(),
+                        this.driveMotors[driveMotorName.ordinal()].getCurrentPosition());
             }
             telemetry.update();
         }
@@ -100,8 +92,8 @@ public abstract class baseRobot extends LinearOpMode {
     }
 
     public void driveInches(double inches, double power) {
-        int[] target = new int[this.driveMotors.size()];
-        double[] powers = new double[this.driveMotors.size()];
+        int[] target = new int[this.driveMotors.length];
+        double[] powers = new double[this.driveMotors.length];
         Arrays.fill(target, (int) this.inchesToEncoder(inches));
         Arrays.fill(powers, power);
 
@@ -109,7 +101,7 @@ public abstract class baseRobot extends LinearOpMode {
     }
 
     public void turnDegrees(int degrees, double power) {
-        int[] target = new int[this.driveMotors.size()];
+        int[] target = new int[this.driveMotors.length];
         double[] powers = new double[] {power, power, -power, -power};
         Arrays.fill(target, (int) this.inchesToEncoder(Math.toRadians(degrees) * this.robotDiameter));
 
