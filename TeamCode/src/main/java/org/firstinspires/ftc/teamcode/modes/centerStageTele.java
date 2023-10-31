767b5ea36d8e856afd973b10b36a9444e5925668
Alex McKinley <alex.mckinley@intel.com>
2023-10-30 20:51:33 -0700

    Create basic classes.
    baseRobot is a class that can be used year after year with only
    small changes. For now, it has driving by encoders. Eventually,
    other methods such as IMU can be used.
    
    CenterStageRobot is this year's competition bot. Because of
    the drive train it has, it can strafe, so that was added.
    
    In the modes module, we have our two runOpMode implementations.
    The teleop one will have all of the button presses and the
    autonomous one will just be a list of instructions and
    censor reads.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modes/centerStageTele.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.modes;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.robot.centerStageRobot;
+@TeleOp(name="Teleop Controls", group ="CenterStage")
+public class centerStageTele extends centerStageRobot {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/centerStageRobot.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+import java.util.Arrays;
+
+@Disabled
+public abstract class centerStageRobot extends baseRobot {
+    public centerStageRobot() {
+        super(3, 15);
+    }
+
+
+    public void driveStrafe(double inches, double power) {
+        int[] target = new int[this.driveMotors.size()];
+        //TODO: test this and make sure that these are the right changes
+        double[] powers = new double[] {power, -power, -power, power};
+        Arrays.fill(target, (int) this.inchesToEncoder(inches));
+
+        driveEncoded(target, powers);
+    }
+}
