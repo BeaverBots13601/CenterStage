a35d3373c6bfce6d554a282e7103bc7de609f8f5
Alex McKinley <alex.mckinley@intel.com>
2023-10-30 20:51:33 -0700

    Create basic classes.
    baseRobot is a class that can be used year after year with only
    small changes. For now, it has driving by encoders. Eventually,
    other methods such as IMU can be used.
    
    CenterStageRobot is this year's competition bot. Because of
    the drive train it has, it can strafe, so that was added.
    
    In the modes module, we have our two runOpMode implementations.
    The teleop one will have all of the button presses and the
    autonomous one will just be a list of instructions and
    censor reads.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/constants.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+public class constants {
+    public static final String[] DRIVE_MOTOR_NAMES = {"leftFront", "leftBack", "rightFront", "rightBack"};
+    public static final int ENCODER_TICKS = 1440;
+    public static final int TELEMETRY_MS_TRANSMISSION_INTERVAL = 25;
+
+
+}
