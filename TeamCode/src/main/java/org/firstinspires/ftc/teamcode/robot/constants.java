aad47488676b26a1b9caaa9e696e0ac62e7fb08c
WHS Robotics <woodlandroboticsteam@woodlandschools.org>
2023-11-03 19:07:37 -0700

    11/3/23 work (minor changes, auto sleeping, base of adjustable speeds (working on more @ home), implement ftcdashboard)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/constants.java
@@ -1,8 +1,12 @@
 package org.firstinspires.ftc.teamcode.robot;
 
 
+import com.acmerobotics.dashboard.config.Config;
 
+@Config
 public class constants {
+    public static int FAR_WAIT_TEAMMATE_MILLISECONDS = 7500;
+
     public enum driveMotorName {
         leftFront, leftBack, rightFront, rightBack;
 
@@ -14,5 +18,15 @@ public class constants {
     public static final double ENCODER_TICKS = 537.70;
     public static final int TELEMETRY_MS_TRANSMISSION_INTERVAL = 25;
 
+    public static double AUTO_PUSH_PIX_FORWARD_DIST_INCHES = 4.5;
 
+    public enum SPEEDS {
+        NORMAL,
+        FAST,
+        CUSTOM_FTC_DASHBOARD
+    }
+
+    public static final double NORMAL_SPEED = 0.65;
+    public static final double FAST_SPEED = 0.85;
+    public static double CUSTOM_FTC_DASHBOARD_SPEED = 0.65;
 }
